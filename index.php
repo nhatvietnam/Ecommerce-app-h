9728f3f4aaec192420c85b9019e7a26a63a2449a
Nhatvietnam <nguyen.nhat.france@gmail.com>
2022-05-08 07:00:30 +0700

    GitHub: Logout

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION["uid"])){
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Ecommerce</title>
+		<title>Thuong mai dien tu</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css"/>
 		<script src="js/jquery2.js"></script>
 		<script src="js/bootstrap.min.js"></script>
@@ -22,7 +22,7 @@ if(isset($_SESSION["uid"])){
 	<div class="loader"></div>
 </div>
 	<div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container-fluid">	
+		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
 					<span class="sr-only">navigation</span>
@@ -93,7 +93,7 @@ if(isset($_SESSION["uid"])){
 			</ul>
 		</div>
 	</div>
-</div>	
+</div>
 	<p><br/></p>
 	<p><br/></p>
 	<p><br/></p>
@@ -151,51 +151,3 @@ if(isset($_SESSION["uid"])){
 	</div>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
